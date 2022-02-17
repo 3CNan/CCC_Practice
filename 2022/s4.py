4a698bbe452438863c6b534419a326fe48c93c43
3CNan <wunanxi257@gmail.com>
2022-02-17 09:48:37 -0500

    Update s4.py

--- 2022/s4.py
+++ 2022/s4.py
@@ -3,8 +3,10 @@ N, C = input().split()
 points = list(map(int, input().split()))
 ans = []
 for comb in itertools.combinations(points, 3):
-    if comb[0] != comb[1] and comb[1] != comb[2] and comb[0] != comb[2]:
-        comb = list(comb)
+    if comb[0] == comb[1] or comb[1] == comb[2] or comb[0] == comb[2]:
+        continue
+    else:
+        # comb = list(comb)
         comb.sort()
         if abs(comb[0] - comb[1]) >= int(C) / 2 or int(C) - abs(comb[0] - comb[2]) >= int(C) / 2 or abs(comb[1] - comb[2]) >= int(C) / 2:
             pass
